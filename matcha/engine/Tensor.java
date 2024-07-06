2b41600963c381df781c3d4f027dbb5d8e070aa1
andrew <aly37@case.edu>
2024-07-06 10:55:58 -0700

    basic tensor functionalities

--- matcha/engine/Tensor.java
+++ matcha/engine/Tensor.java
@@ -1,5 +1,47 @@
 package matcha.engine;
 
-public class Tensor<T> {
+import matcha.utils.math.LinAlg;
+import java.util.Arrays;
+
+/**
+ * Tensor - in accordance with its more primitive predecessor Value, the core data structure behind matcha operations.
+ * @author andrewye
+ */
+public class Tensor {
+    private int[] shape;
+    private double[] data; // data is stored as a 1-d array in memory, with shapes being row-major indexed.
+
+    /**
+     * Creates a zero-initialized tensor.
+     * @shape the shape of the tensor.
+     */
+    public Tensor(int[] shape) {
+        this.shape = shape;
+
+        int numElements = 1;
+        for (int i = 0; i < shape.length; i++)
+            numElements *= shape[i];
+        this.data = new double[numElements];
+    }
+
+    public Tensor(Object d){
+        this.shape = LinAlg.getDims(d);
+    }
+
+    public double get(int[] idxs) throws Exception {
+        return data[LinAlg.indexRowMajor(data, shape, idxs)];
+    }
+
+    public void set(int[] idxs, double x) throws Exception {
+        data[LinAlg.indexRowMajor(data, shape, idxs)] = x;
+    }
+
+    public double[] data() {
+        return data;
+    }
+
+    public int[] shape() {
+        return shape;
+    }
 
 }
--- matcha/utils/math/LinAlg.java
+++ matcha/utils/math/LinAlg.java
@@ -1,30 +1,143 @@
 package matcha.utils.math;
 
-import matcha.engine.*;;
+import matcha.engine.*;
+import java.util.List;
+import java.util.ArrayList;
 
+/**
+ * LinAlg - implements several useful algebraic manipulations and functions on matrices and tensors.
+ * @author  andrewye
+ */
 public class LinAlg {
-    public static double[][] eye(int dim){
+    /** 
+     * Returns a standard square identity matrix.
+     * @param dim the number of rows (or cols).
+     * @return An identity matrix of dimensions dim*dim.
+     */
+    public static double[][] eye(int dim) {
         double[][] I = new double[dim][dim];
-        
-        for(int i = 0; i < dim; i++){
-            for(int j = 0; j < dim; j++){
-                if (i == j) I[i][j] = 1.0;
+
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                if (i == j)
+                    I[i][j] = 1.0;
             }
         }
 
         return I;
     }
 
-    public static double[][] diagFlat(Value[] v){
+    /**
+     * Returns the Row-Major index of an element in a list of elements, parametrized by the given shape and in-shape index.
+     * For more information, see https://en.wikipedia.org/wiki/Row-_and_column-major_order#
+     * @param elements the list of elements.
+     * @param shape the shape in which to parametrize the elements.
+     * @param idxs the shape-indices of the element to retrieve.
+     * @return The index of the list element associated at the shape index in row-major order.
+     */
+    public static int indexRowMajor(double[] elements, int[] shape, int[] idxs) throws Exception {
+        if (shape.length != idxs.length) {
+            throw new Exception("Error: number of indexes must match shape of object!");
+        } 
+
+        int elementsInShape = 1;
+        for(int i = 0; i < shape.length; i++) elementsInShape *= shape[i];
+        if (elements.length != elementsInShape){
+            throw new Exception("Error: the number of elements specified by the shape does not match the data.");
+        }
+
+        for (int i = 0; i < idxs.length; i++) {
+            if (idxs[i] < 0 || idxs[i] >= shape[i]) {
+                System.out.println("Warning: indices are inconsistent with the shape of the data.");
+            }
+        }
+
+        int n_i = 0;
+        int N_i = 1;
+        int rmo = idxs[n_i];
+
+        while (N_i < shape.length) {
+            rmo = shape[N_i++] * rmo + idxs[++n_i];
+        }
+
+        return rmo;
+    }
+    
+    /**
+     * Returns a 2-D square matrix with the elements of input as the diagonal.
+     * @param v the input elements to be diagonalized.
+     * @return diagonalized matrix of the input vector.
+     */
+    public static double[][] diagFlat(Value[] v) {
         double[][] A = new double[v.length][v.length];
-        
-        for(int i = 0; i < v.length; i++){
-            for(int j = 0; j < v.length; j++){
-                if (i == j) A[i][j] = v[i].data();
+
+        for (int i = 0; i < v.length; i++) {
+            for (int j = 0; j < v.length; j++) {
+                if (i == j)
+                    A[i][j] = v[i].data();
             }
         }
 
         return A;
     }
+    
+    /**
+     * Returns the dimensions of an array (or nested array) of objects.
+     * @param the object to retrieve the shape of.
+     * @return shape of the object.
+     */
+    public static int[] getDims(Object v){
+        return getDims(v, new ArrayList<Integer>());
+    }
 
-}   
+    private static int[] getDims(Object v, List<Integer> dims) {
+        if (v instanceof Object[]) {
+            Object[] v_arr = (Object[]) v;
+            dims.add(v_arr.length);
+            return (v_arr.length == 0) ? dims.stream().mapToInt(x -> x).toArray() : getDims(v_arr[0], dims);
+        }
+        if (v instanceof byte[]) {
+            byte[] v_arr = (byte[]) v;
+            dims.add(v_arr.length);
+            return (v_arr.length == 0) ? dims.stream().mapToInt(x -> x).toArray() : getDims(v_arr[0], dims);
+        }
+        if (v instanceof short[]) {
+            short[] v_arr = (short[]) v;
+            dims.add(v_arr.length);
+            return (v_arr.length == 0) ? dims.stream().mapToInt(x -> x).toArray() : getDims(v_arr[0], dims);
+        }
+        if (v instanceof int[]) {
+            int[] v_arr = (int[]) v;
+            dims.add(v_arr.length);
+            return (v_arr.length == 0) ? dims.stream().mapToInt(x -> x).toArray() : getDims(v_arr[0], dims);
+        }
+        if (v instanceof long[]) {
+            long[] v_arr = (long[]) v;
+            dims.add(v_arr.length);
+            return (v_arr.length == 0) ? dims.stream().mapToInt(x -> x).toArray() : getDims(v_arr[0], dims);
+        }
+        if (v instanceof float[]) {
+            float[] v_arr = (float[]) v;
+            dims.add(v_arr.length);
+            return (v_arr.length == 0) ? dims.stream().mapToInt(x -> x).toArray() : getDims(v_arr[0], dims);
+        }
+        if (v instanceof double[]) {
+            double[] v_arr = (double[]) v;
+            dims.add(v_arr.length);
+            return (v_arr.length == 0) ? dims.stream().mapToInt(x -> x).toArray() : getDims(v_arr[0], dims);
+        }
+        if (v instanceof boolean[]) {
+            boolean[] v_arr = (boolean[]) v;
+            dims.add(v_arr.length);
+            return (v_arr.length == 0) ? dims.stream().mapToInt(x -> x).toArray() : getDims(v_arr[0], dims);
+        }
+        if (v instanceof char[]) {
+            char[] v_arr = (char[]) v;
+            dims.add(v_arr.length);
+            return (v_arr.length == 0) ? dims.stream().mapToInt(x -> x).toArray() : getDims(v_arr[0], dims);
+        }
+
+        return dims.stream().mapToInt(x -> x).toArray();
+    }
+
+}
